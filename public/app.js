6b026251f598fb9147eba13e33908a92fec7b007
SahilArote <arotesahil2@gmail.com>
2026-04-13 16:48:49 +0530

    render push

--- public/app.js
+++ public/app.js
@@ -1,4 +1,6 @@
-const API = 'http://localhost:3000/api';
+const API = window.location.hostname === 'localhost'
+    ? 'http://localhost:3000/api'
+    : `${window.location.origin}/api`;
 let token = localStorage.getItem('token');
 let currentUser = null;
 let currentChatId = null;
